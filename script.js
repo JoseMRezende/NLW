f22b409fe5299e98aba7443025dfbfdc49d46b69
JoseMRezende <rezendinhobr18@gmail.com>
2023-01-19 20:19:54 -0300

    Stage 4.0

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-02', '01-03'],
-    water: ['01-01', '01-03'],
-    food: ['01-02', '01-03'],
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+    if (dayExists) {
+        alert("Dia já incluso ⛔")
+        return
+    }
+    alert('Adicionado com sucesso ✅')
+    nlwSetup.addDay(today)
 }
 
+function save() {
+    localStorage.setItem('NLW@Setup', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLW@Setup")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
