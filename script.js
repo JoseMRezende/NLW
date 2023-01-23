c9916f17e5d8e023f90f8b739ed650f22e1c65a6
JoseMRezende <rezendinhobr18@gmail.com>
2023-01-23 10:25:52 -0300

    Adding personal customizations stage 1.0

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ button.addEventListener('click', add)
 form.addEventListener("change", save)
 
 function add() {
-    const today = "18/01" //new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
     const dayExists = nlwSetup.dayExists(today)
     if (dayExists) {
         alert("Dia já incluso ⛔")
